443a15c5523a0f28273126b85ba78c42c11c8b1e
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-04-11 22:26:54 +0300

    feat: implement matrix-elements-sum task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,17 +14,18 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
-  // let sum = 0;
-  // for (let i = 0; i < matrix.length; i++) {
-  //   for (let j = 0; j < matrix[i].length; j++) {
-  //     if (matrix[i - 1][j] !== 0) {
-  //       sum += matrix[i][j];
-  //     }
-  //   }
-  // }
-  // return sum;
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  const columnCount = matrix[0].length;
+  for (let j = 0; j < columnCount; j++) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[i][j] === 0) {
+        break;
+      }
+      sum += matrix[i][j];
+    }
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
