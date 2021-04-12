3efc875f65022ecd37e67c57a0b3074f3c9fd3ce
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-04-12 22:48:16 +0300

    feat mine sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,44 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const array = [];
+  for (let i = 0; i < matrix.length; i++) {
+    array.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+      if (i > 0) {
+        if (matrix[i - 1][j]) {
+          count += 1;
+        }
+        if (matrix[i - 1][j + 1]) {
+          count += 1;
+        }
+        if (matrix[i - 1][j - 1]) {
+          count += 1;
+        }
+      }
+      if (i < matrix.length - 1) {
+        if (matrix[i + 1][j]) {
+          count += 1;
+        }
+        if (matrix[i + 1][j + 1]) {
+          count += 1;
+        }
+        if (matrix[i + 1][j - 1]) {
+          count += 1;
+        }
+      }
+      if (matrix[i][j + 1]) {
+        count += 1;
+      }
+      if (matrix[i][j - 1]) {
+        count += 1;
+      }
+      array[i].push(count);
+    }
+  }
+  return array;
 }
 
 module.exports = minesweeper;
